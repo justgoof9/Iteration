80268a2f3f66638511e0d34ef98ef8c24ac67656
justgoof9 <justgoof9@gmail.com>
2024-03-12 18:58:50 -0400

    Added delete feature

--- Javascript/Manager/manager.js
+++ Javascript/Manager/manager.js
@@ -1,16 +1,16 @@
 //Time slot Object for Manager page.
 let timeSlots = {
     "Producer1": [
-        {name : "John Doe", date: "3/10/2024", start: "1:00pm", end: "3:00pm"},
-        {name : "Jane Doe", date: "3/11/2024", start: "4:00pm", end: "5:00pm"},
-        {name : "Willow Smith", date: "3/12/2024", start: "6:00pm", end: "7:00pm"},
+        { name: "John Doe", date: "3/10/2024", start: "1:00pm", end: "3:00pm" },
+        { name: "Jane Doe", date: "3/11/2024", start: "4:00pm", end: "5:00pm" },
+        { name: "Willow Smith", date: "3/12/2024", start: "6:00pm", end: "7:00pm" },
     ],
     "Producer2": [
-        {name : "Bruce Wayne", date: "3/15/2024", start: "8:00pm", end: "9:00pm"},
-        {name : "Frederick Barbarossa", date: "3/16/2024", start: "10:00pm", end: "11:00pm"},
+        { name: "Bruce Wayne", date: "3/15/2024", start: "8:00pm", end: "9:00pm" },
+        { name: "Frederick Barbarossa", date: "3/16/2024", start: "10:00pm", end: "11:00pm" },
     ],
     "Producer3": [
-        {name : "Catherine de'Medici", date: "3/17/2024", start: "12:00am", end: "1:00am"},
+        { name: "Catherine de'Medici", date: "3/17/2024", start: "12:00am", end: "1:00am" },
     ]
 }
 
@@ -26,6 +26,13 @@ window.addEventListener('load', initalizePage);
 const printTimeSlots = () => {
     console.log(timeSlots);
 }
+//Delete DJ object.
+const deleteDj = (producer, index) => {
+    if (confirm("Are you sure to delete this DJ's time?")) {
+        timeSlots[producer].splice(index, 1);
+        displayTimeSlots(producer);
+    }
+}
 
 //Displaying producer from the object array.
 const displayTimeSlots = (pd) => {
@@ -33,7 +40,7 @@ const displayTimeSlots = (pd) => {
     const listOfTimeSlots = document.querySelector('.list-of-djs ul');
     listOfTimeSlots.innerHTML = '';
 
-    if(timeSlots[pd]) {
+    if (timeSlots[pd]) {
         timeSlots[pd].forEach((dj) => {
             const listItem = document.createElement('li');
             listItem.innerHTML = `<span>${dj.name} Date: ${dj.date} Start: ${dj.start} End: ${dj.end} </span>
@@ -43,10 +50,20 @@ const displayTimeSlots = (pd) => {
                            <div>;`
             listOfTimeSlots.appendChild(listItem);
         });
-    }else {
+
+        //Adding delete event listener
+        document.querySelectorAll('.delete-btn').forEach((deleteBtn) => {
+            deleteBtn.addEventListener('click', (e) => {
+                const djDeleteIndex = parseInt(e.target.dataset.index, 10);
+                deleteDj(pd, djDeleteIndex);
+
+            });
+        });
+    } else {
         console.error(`No timeslots for the ${pd} found`);
     }
 }
+
 //Adding eventListener to each Producer list.
 document.querySelectorAll('.producer-list ul li').forEach((producerDJ) => {
     producerDJ.addEventListener('click', (e) => {
@@ -60,21 +77,23 @@ document.querySelectorAll('.producer-list ul li').forEach((producerDJ) => {
 let selectedProducer = "Producer1";
 let selectedDJIndex = 0;
 
+
+
 //Editing the DJ object.
 const editDJModal = (producer, djIndex) => {
     const selectedDJ = timeSlots[producer][djIndex];
     selectedProducer = producer;
     selectedDJIndex = djIndex;
-    // Log to check if the function is called
+    //Log to check if the function is called
     console.log('Open Edit Modal:', selectedDJ);
 
-    // Fill the form with the selected DJ's values
+    //Fill the form with the selected DJ's values
     document.getElementById('djName').value = selectedDJ.name;
     document.getElementById('djDate').value = selectedDJ.date;
     document.getElementById('djStartTime').value = selectedDJ.start;
     document.getElementById('djEndTime').value = selectedDJ.end;
 
-    // Display the modal
+    //Display the modal
     const modal = document.getElementById('djModal');
     console.log('Modal Element:', modal);
     modal.showModal();
@@ -84,32 +103,32 @@ document.addEventListener('click', (event) => {
     const target = event.target;
 
     if (target.classList.contains('edit-btn')) {
-        // Traverse up the DOM to find the closest 'li' element
+        // Traverse up the DOM to find the closest li element
         const listItem = target.closest('li');
 
-        // Check if the listItem exists
+        //Check if the listItem exists
         if (listItem) {
-            // Find the index of the listItem among its siblings
+            //Find the index of the listItem among its siblings
             const index = Array.from(listItem.parentElement.children).indexOf(listItem);
 
-            // Get the selected producer based on the index
+            //Get the selected producer based on the index
             const selectedProducer = 'Producer' + (index + 1);
 
-            // Call the editDJModal function with the selected information
+            //Call the editDJModal function with the selected information
             editDJModal(selectedProducer, index);
         }
     }
 });
 
 
-// Close DJ modal screen.
+//Close DJ modal screen.
 const closeDJModal = () => {
     const modal = document.getElementById('djModal');
 
     //check if modal exists
-    if(modal) {
+    if (modal) {
         modal.close()
-    }else {
+    } else {
         console.log("DJ Modal not found!");
     }
 }
@@ -121,30 +140,31 @@ const saveDJ = () => {
     const djStartTime = document.getElementById('djStartTime').value;
     const djEndTime = document.getElementById('djEndTime').value;
 
-    //Make sure all the input fields are filled.
-    if(!djName || !djDate || !djStartTime || !djEndTime) {
+    //Check all the input fields are filled.
+    if (!djName || !djDate || !djStartTime || !djEndTime) {
         alert("Fill in all the fields!");
         return;
     }
 
     //Update the object.
-    if(selectedDJIndex !== null && selectedDJIndex !== -1) {
-        if(selectedProducer) {
+    if (selectedDJIndex !== null && selectedDJIndex !== -1) {
+        if (selectedProducer) {
             timeSlots[selectedProducer][selectedDJIndex] = {
-                name : djName,
-                date : djDate,
-                start : djStartTime,
-                end : djEndTime
+                name: djName,
+                date: djDate,
+                start: djStartTime,
+                end: djEndTime
             };
-        
 
-        closeDJModal();
+            console.log("Updated DJ: ", timeSlots[selectedProducer][selectedDJIndex]);
 
-        displayTimeSlots(selectedProducer);
-        }else {
+            closeDJModal();
+
+            displayTimeSlots(selectedProducer);
+        } else {
             console.log("Invalid Producer selected");
         }
-    }else {
+    } else {
         console.log("Invalid DJ info");
     }
 }
@@ -152,6 +172,7 @@ const saveDJ = () => {
 //To make this function accesible from global scope.
 window.closeDJModal = closeDJModal;
 window.saveDJ = saveDJ;
+
 //For login page.
 const validLogin = (e) => {
     e.preventDefault();
@@ -160,10 +181,10 @@ const validLogin = (e) => {
     const user = document.getElementById('username');
     const pass = document.getElementById('password');
 
-    if(user.value.trim().length >= 5 && pass.value.trim().length >= 5) {
+    if (user.value.trim().length >= 5 && pass.value.trim().length >= 5) {
         alert("Login sucessful!");
         window.location.href = './Manager.html'
-    }else {
+    } else {
         alert("Mininum 5 characters long");
 
     }
